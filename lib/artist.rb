a57ebd5cd3cacdc65f6c70a0eb4911f6495f2862
Edsel Caprice <caprice@mail.usf.edu>
2018-07-08 01:56:08 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,11 +5,18 @@ class Artist
     @name = name
     @@all << self
   end
-  
+
   @@all = []
 
   def self.all
     @@all
   end
+
+  def new_song(name, genre)
+    song = Song.new(name,genre)
+    song.artist = self
+  end
+
+def 
 
 end
